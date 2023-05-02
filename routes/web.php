5e7a5b960416d91632618a43e58202962ce00a95
NicholasConti <conti94@live.it>
2023-05-02 19:32:39 +0200

    navbar

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'list' => config('comics')
+    ];
+    return view('home', $data);
 });
