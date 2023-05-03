e368c360577ccd5a38e78d3c0128669e0b4bd8ea
NicholasConti <conti94@live.it>
2023-05-03 08:13:10 +0200

    footer-top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,91 @@
 <footer>
-    <h3>footer</h3>
+    <div class="more-liks">
+        <div>
+        <ul class="list-credits">
+            <li class="title-credit">DC COMICS
+                <ul>
+                    <li class="subtitle"><a href="#">Characters</a>
+                    </li>
+                    <li class="subtitle"><a href="#">Comics</a>
+                    </li>
+                    <li class="subtitle"><a href="#">Movies</a>
+                    </li>
+                    <li class="subtitle"><a href="#">Tv</a>
+                    </li>
+                    <li class="subtitle"><a href="#">Games</a>
+                    </li>
+                    <li class="subtitle"><a href="#">Videos</a>
+                    </li>
+                    <li class="subtitle"><a href="#">News</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="title-credit">SHOP
+                <ul>
+                    <li class="subtitle"><a href="#">Shop DC</a>
+                    </li>
+                    <li class="subtitle"><a href="#">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="title-credit">DC
+                <ul>
+                    <li>
+                        <a href="#" class="subtitle">Terms Of Use</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">Privacy policy (New)</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">Talent Workshop</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">CPSC Certificates</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="#"class="subtitle">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">Contact Us</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="title-credit">SITES
+                <ul>
+                    <li>
+                        <a href="#" class="subtitle">DC</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="#" class="subtitle">DC Power Visa</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+        </div>
+    </div>
 </footer>
