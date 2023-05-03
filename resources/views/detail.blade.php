23ebd9bd86190abd88e4d0bf7a336eb6164e9f92
NicholasConti <conti94@live.it>
2023-05-03 16:44:15 +0200

    available section

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -10,7 +10,25 @@
         <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
     </div>
 </div>
-<div class="cnt-detail">
-   
+<div class="cnt-detail card-data">
+   <div>
+        <h2>
+            {{ $comic['title'] }}
+        </h2>
+        <div class="btn-green">
+            <div class="btn-green price">
+                <div>U.S. Price: <span>{{ $comic['price'] }}</span></div>
+                <div>AVAILABLE</div>
+            </div>
+            <div class="btn-green">
+                <span>Check Availability</span> 
+            </div>
+        </div>
+        <p>{{ $comic['description'] }}</p>
+   </div>
+   <div>
+    <h6>ADVERTISMENT</h6>
+    <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="adv">
+   </div>
 </div>
 @endsection
