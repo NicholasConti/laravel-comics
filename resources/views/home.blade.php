1594d9fd30212ae3c0d3f7cecd85e8e35f7af92a
NicholasConti <conti94@live.it>
2023-05-03 07:49:37 +0200

    main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,5 +20,39 @@ Home
         <div class="load">
             <button>LOAD MORE</button>
         </div>
+        <div class="merch-bar">
+            <ul>
+                <li>
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                        Digital Comics
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                        DC Merchandise
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                        Subscription
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                        Comic Shop Location
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img class="small-img" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                        DC Power Visa
+                    </a>
+                </li>
+            </ul>
+        </div>
     </section>
 @endsection
