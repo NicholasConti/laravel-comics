257f2e3c3adf80cd8c0b689442e37f30a6499d1b
NicholasConti <conti94@live.it>
2023-05-03 07:29:08 +0200

    card-list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,24 @@
 @extends('layouts.app');
+
+@section('page.title')
+Home
+@endsection
+@section('page.main')
+    <section>
+        <div>
+                @foreach ($comics as $comic)
+                <div class="product">
+                    <div>
+                        <img src="{{ $comic['thumb'] }}" alt="">
+                    </div>
+                    <div>
+                        <h3>{{ $comic['title'] }}</h3>
+                    </div>
+                </div>
+                @endforeach
+        </div>
+        <div class="load">
+            <button>LOAD MORE</button>
+        </div>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $data = [
-        'list' => config('comics')
+        'comics' => config('comics')
     ];
     return view('home', $data);
 });
